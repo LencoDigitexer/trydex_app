14094cb6676332c56a2021c6c87ef927fc323aef
LencoDigitexer <addminuser258@gmail.com>
2023-04-17 02:51:43 +0300

    upload

---
+++ app/src/main/java/org/disroot/disrootapp/utils/Contributors.java
@@ -0,0 +1,19 @@
+package org.disroot.disrootapp.utils;
+
+/**
+ * Created by jackson on 02/11/15.
+ */
+public class Contributors {
+    public static final String[] admins= {
+            "<a href='https://github.com/lencodigitexer'>lencodigitexer"
+    };
+    public static final String[] devs= {
+            "<a href='https://github.com/lencodigitexer'>lencodigitexer"
+    };
+    public static final String[] translators= {
+            "<a href='https://github.com/lencodigitexer'>lencodigitexer"
+    };
+    public static final String[] artworks= {
+            "<a href='https://github.com/lencodigitexer'>lencodigitexer"
+    };
+}
